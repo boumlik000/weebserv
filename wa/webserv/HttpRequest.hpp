810b335225087f24dc645b8b46b1e69b9cc1fd71
rlamtaou <rlamtaou@c4r9p6.1337.ma>
2025-09-02 11:02:33 +0100

    sa

---
+++ wa/webserv/HttpRequest.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <map>
+
+#define MAX_REQUEST 8192
+
+
+
+
+
+class HttpRequest {
+public:
+    // === Orthodox Canonical Form (OCF) ===
+    HttpRequest();
+    HttpRequest(const HttpRequest& src);
+    HttpRequest& operator=(const HttpRequest& rhs);
+    ~HttpRequest();
+
+    // === الدالة الرئيسية للتحليل ===
+    bool parse(const std::string& raw_request);
+
+    // === Getters باش نجبدو المعلومات ===
+    const std::string& getMethod() const;
+    const std::string& getUri() const;
+    const std::string& getHttpVersion() const;
+    const std::string getHeader(const std::string& key) const;
+    const std::map<std::string, std::string>& getHeaders() const;
+    const std::vector<char>& getBody() const;
+
+private:
+    // === المتغيرات الأعضاء ===
+    std::string                         _method;
+    std::string                         _uri;
+    std::string                         _http_version;
+    std::map<std::string, std::string>  _headers;
+    std::vector<char>                   _body;
+
+    // === الدوال المساعدة الخاصة ===
+    void _parseRequestLine(const std::string& line);
+    void _parseHeader(const std::string& line);
+};
